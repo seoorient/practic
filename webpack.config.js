176d9ce2822bcc3948d0306abaedef1a6cc7e949
seoorient <seoorient@gmail.com>
2020-05-17 18:19:10 +0300

    finish css layout

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,8 @@ const jsLoaders = () => {
       loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env']
-      },
-    },
+      }
+    }
   ]
 
   if (isDev) {
@@ -32,19 +32,19 @@ module.exports = {
   entry: ['@babel/polyfill', './index.js'],
   output: {
     filename: filename('js'),
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     extensions: ['.js'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
-    },
+      '@core': path.resolve(__dirname, 'src/core')
+    }
   },
   devtool: isDev ? 'source-map' : false,
   devServer: {
     port: 3000,
-    hot: isDev,
+    hot: isDev
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -52,18 +52,18 @@ module.exports = {
       template: 'index.html',
       minify: {
         removeComments: isProd,
-        collapseWhitespace: isDev,
-      },
+        collapseWhitespace: isProd
+      }
     }),
     new CopyPlugin([
       {
         from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist'),
-      },
+        to: path.resolve(__dirname, 'dist')
+      }
     ]),
     new MiniCssExtractPlugin({
-      filename: filename('css'),
-    }),
+      filename: filename('css')
+    })
   ],
   module: {
     rules: [
@@ -74,19 +74,18 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
             options: {
               hmr: isDev,
-              reloadAll: true,
-            },
+              reloadAll: true
+            }
           },
           'css-loader',
-          'sass-loader',
+          'sass-loader'
         ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: jsLoaders(),
-
-      },
-    ],
-  },
+        use: jsLoaders()
+      }
+    ]
+  }
 }
